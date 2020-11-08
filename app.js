d97a1cbe3a41b552fec22b8aa6b94226ca8f639c
Shubham Khantwal <skhantwal84@gmail.com>
2020-11-08 14:52:41 +0530

    add front end

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-//app.use(express.static(__dirname));
+app.use(express.static(__dirname+'/page.html'));
 
 const fPage = path.resolve(__dirname+"../redis-monitor/app/templates/index_page.html");
 
 app.get('/',(req,res)=>{
- res.end("FRONT PAGE")
+ res.end("FRONT-PAGE")
 });
 
 app.get('/api/redis_list',(req,res)=>{
-    
+
 });
 app.get('/api/redis_info',(req,res)=>{
 
@@ -33,7 +33,7 @@ app.get('/api/ping',(req,res)=>{
 });
 
 app.post('/api/add',(req,res)=>{
-
+    
 });
 app.post('/api/del',(req,res)=>{
 
