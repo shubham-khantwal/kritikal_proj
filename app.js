d1553926773ba3f15ffa09a3de066526dcc336d3
Shubham Khantwal <skhantwal84@gmail.com>
2020-11-08 18:34:32 +0530

    update

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-var urlencodedParser = bodyParser.urlencoded({ extended: false }) 
-const redis = require('redis');
-const db = require('./models');
+// var urlencodedParser = bodyParser.urlencoded({ extended: false }) 
+//const redis = require('redis');
+const db = require('./models/statsinformation');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 const REDIS_PORT  = process.env.PORT || 8000;
 
+const sPath = path.join(__dirname,'/static/index.html');
 app.use(bodyParser.json());
-app.use(express.static(__dirname+'/page.html'));
+app.use(express.static(sPath));
 
 
 // create client
@@ -20,9 +21,12 @@ app.use(express.static(__dirname+'/page.html'));
 
 
 app.get('/',(req,res)=>{
- res.end("FRONT-PAGE");
+ res.sendFile(sPath);
 });
 
+app.post('/',(req,res)=>{
+    res.redirect('/api/add');
+});
 app.get('/api/redis_list',(req,res)=>{
     res.end("Will print data from redis");
 });
@@ -38,18 +42,18 @@ app.get('/api/ping',(req,res)=>{
 
 // lets add data to sqlite database
 app.post('/api/add',(req,res)=>{
-    const {connect_received , cmd_processed, ops_per_sec , rejected_connect , expired_keys ,evicted_keys,hits,misses} = req.body;
-    return db.statsInformation.create({connect_received , cmd_processed, ops_per_sec , rejected_connect , expired_keys ,evicted_keys,hits,misses})
+    const {connect_received , cmd_processed, ops_per_sec , rejected_connect , expired_keys ,evicted_keys,hits,misses} = req.body
+    return db.create({connect_received , cmd_processed, ops_per_sec , rejected_connect , expired_keys ,evicted_keys,hits,misses})
     .then((stats)=>res.send(stats))
     .catch((err)=>{
-        console.log('*** There in entering data ***', JSON.stringify(stats));
+        console.log('*** There is an error ***', JSON.stringify(stats));
         return res.status(400).send(err);
     })
 });
 //lets remove the data
 app.post('/api/del/:id',(req,res)=>{
     const id = parseInt(req.params.id)
-    return db.statsInformation.findById(id)
+    return db.findById(id)
     .then((stats)=>stats.destroy())
     .then(()=>res.send({id}))
     .catch((err)=>{
